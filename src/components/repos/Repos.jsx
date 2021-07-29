1fe87df45172b95cf80979393140932fed37aca0
PriyankaBelhekar77 <68643529+PriyankaBelhekar77@users.noreply.github.com>
2021-07-29 19:19:54 +0530

    Update Repos.jsx

--- src/components/repos/Repos.jsx
+++ src/components/repos/Repos.jsx
@@ -4,7 +4,6 @@ import propTypes from 'prop-types';
 import ReposItem from './ReposItem';
 
 const Repos = ({ repos }) => {
-  console.log('repos', repos);
   return repos.map((repo) => {
     <ReposItem repo={repo} key={repo.id} />
   });
